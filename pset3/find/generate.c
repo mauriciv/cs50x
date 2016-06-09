f980ad945746a33e740ecc385a2d60cc3dc71387
Mauricio <veramauricio92@gmail.com>
2016-06-08 21:51:33 -0400

    Added the missing comments

--- pset3/find/generate.c
+++ pset3/find/generate.c
@@ -24,17 +24,17 @@
 
 int main(int argc, string argv[])
 {
-    // TODO: comment me
+    // If the number of command line arguments is not 2 or 3
     if (argc != 2 && argc != 3)
     {
         printf("Usage: generate n [s]\n");
         return 1;
     }
 
-    // TODO: comment me
+    // Cast the second argument to an int
     int n = atoi(argv[1]);
 
-    // TODO: comment me
+    // Seed the rng if seed was provided
     if (argc == 3)
     {
         srand48((long int) atoi(argv[2]));
@@ -44,7 +44,7 @@ int main(int argc, string argv[])
         srand48((long int) time(NULL));
     }
 
-    // TODO: comment me
+    // Print n amount of 'random' numbers
     for (int i = 0; i < n; i++)
     {
         printf("%i\n", (int) (drand48() * LIMIT));
