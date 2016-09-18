9fa04360e0b0d3672ec0df95bb38184ee075fa5b
Mauricio <veramauricio92@gmail.com>
2016-09-18 18:46:22 -0400

    Finish the 'resize' excercise

--- pset4/bmp/resize.c
+++ pset4/bmp/resize.c
@@ -1,12 +1,3 @@
-/**
- * copy.c
- *
- * Computer Science 50
- * Problem Set 4
- *
- * Copies a BMP piece by piece, just because.
- */
-       
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -15,17 +6,24 @@
 int main(int argc, char* argv[])
 {
     // ensure proper usage
-    if (argc != 3)
+    if (argc != 4)
     {
-        printf("Usage: ./copy infile outfile\n");
+        printf("Usage: ./resize size infile outfile\n");
         return 1;
     }
 
-    // remember filenames
-    char* infile = argv[1];
-    char* outfile = argv[2];
+    int size_multiplier = atoi(argv[1]);
+    if (size_multiplier == 0) {
+        printf("Error. size must be a number\n");
+        return 1;
+    }
 
-    // open input file 
+
+    // remember filenames
+    char* infile = argv[2];
+    char* outfile = argv[3];
+
+    // open input file
     FILE* inptr = fopen(infile, "r");
     if (inptr == NULL)
     {
@@ -51,7 +49,7 @@ int main(int argc, char* argv[])
     fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
 
     // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
-    if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 || 
+    if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 ||
         bi.biBitCount != 24 || bi.biCompression != 0)
     {
         fclose(outptr);
@@ -60,39 +58,65 @@ int main(int argc, char* argv[])
         return 4;
     }
 
+    // determine padding for scanlines, in 'pixels'
+    int original_padding =  (( bi.biWidth * sizeof(RGBTRIPLE) ) % 4) * 3;
+
+    int original_height = bi.biHeight;
+    bi.biHeight = bi.biHeight * size_multiplier;
+
+    int original_width = bi.biWidth;
+    bi.biWidth = bi.biWidth * size_multiplier;
+    // The amount of bytes to insert after each scanline in the new image
+    int new_padding = (( bi.biWidth * sizeof(RGBTRIPLE) ) % 4) * 3;
+
+    int pixel_count = abs(bi.biHeight) * bi.biWidth;
+    bi.biSizeImage = pixel_count * sizeof(RGBTRIPLE) + (new_padding * abs(bi.biHeight));
+    bf.bfSize = bi.biSizeImage + sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
+
     // write outfile's BITMAPFILEHEADER
     fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
 
     // write outfile's BITMAPINFOHEADER
     fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
 
-    // determine padding for scanlines
-    int padding =  (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
 
     // iterate over infile's scanlines
-    for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
+    for (int i = 0, biHeight = abs(original_height); i < biHeight; i++)
     {
+
+        RGBTRIPLE pixels[bi.biWidth];
+        int last_free_cell = 0;
+
         // iterate over pixels in scanline
-        for (int j = 0; j < bi.biWidth; j++)
+        for (int j = 0; j < original_width; j++)
         {
             // temporary storage
             RGBTRIPLE triple;
 
             // read RGB triple from infile
             fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
+            // Loop for storing the pixel in the array "size_multiplier" times
+            for (int repeated_column = 0; repeated_column < size_multiplier; repeated_column++) {
+                pixels[last_free_cell] = triple;
+                last_free_cell++;
+            }
 
-            // write RGB triple to outfile
-            fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
+        }
+
+        for (int repeated_row = 0; repeated_row < size_multiplier; repeated_row++) {
+            for (int pixel = 0; pixel < bi.biWidth; pixel++) {
+                // write RGB triple to outfile
+                fwrite(&pixels[pixel], sizeof(RGBTRIPLE), 1, outptr);
+            }
+            // add padding
+            for (int k = 0; k < new_padding; k++) {
+                fputc(0x00, outptr);
+            }
         }
 
         // skip over padding, if any
-        fseek(inptr, padding, SEEK_CUR);
+        fseek(inptr, original_padding, SEEK_CUR);
 
-        // then add it back (to demonstrate how)
-        for (int k = 0; k < padding; k++)
-        {
-            fputc(0x00, outptr);
-        }
     }
 
     // close infile
